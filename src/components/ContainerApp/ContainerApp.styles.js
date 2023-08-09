1d0386e85e6f8d1486467756b55df6326defe481
Juan Manuel Androetto <juan.androetto36@gmail.com>
2023-08-09 12:52:24 -0300

    solucion ancho pantalla

--- src/components/ContainerApp/ContainerApp.styles.js
+++ src/components/ContainerApp/ContainerApp.styles.js
@@ -3,16 +3,16 @@ import { styled } from "../../theme/styled";
 export const StyledContainerApp = styled("div", {
   boxShadow: "2px 2px 60px rgba(0, 0, 0, 0.8)",
   margin: "0 auto",
-  " @media (min-width: 1800px)": {
+  "@media (min-width: 1800px)": {
     maxWidth: "1600px",
   },
-  " @media (max-width: 1600px)": {
+  "@media (max-width: 1600px)": {
     maxWidth: "1200px",
   },
-  " @media (max-width: 1200px)": {
+  "@media (max-width: 1200px)": {
     maxWidth: "1000px",
   },
-  " @media (max-width: 600px)": {
+  "@media (max-width: 600px)": {
     maxWidth: "100%",
   },
 });
